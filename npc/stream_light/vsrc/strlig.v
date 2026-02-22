0072b9c0b98d5228e0d717a3ad19f57a318276a4
tracer-ysyx <tracer@ysyx.org>
2026-02-22 08:11:25 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:11:25 up 12 min,  1 user,  load average: 0.60, 0.19, 0.06

--- npc/stream_light/vsrc/strlig.v
+++ npc/stream_light/vsrc/strlig.v
@@ -6,10 +6,15 @@ module strlig(
   reg [15: 0] temp;
   reg [31: 0] count;
   always@(posedge clk)begin
-	if(rst) temp[15: 0] <= 16'b1;
+	//if(rst) temp[15: 0] <= 16'b1;
+	if(rst) begin
+	  temp[14: 0] <= 15'b0;
+	  temp[15] <= 1;
+	end
 	else begin
 	  count <= (count >= 5000000 ? 32'b0: count + 1);
-	  if(count == 0) temp <= {led[14: 0], led[15]};
+//	  if(count == 0) temp <= {led[14: 0], led[15]};
+	  if(count == 0) temp <= {led[0], led[15: 1]};
 	end
   end
   assign led = temp;
