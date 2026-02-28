fe2d5144a8eb527c4010d55c8bfe6e7634a2511b
tracer-ysyx <tracer@ysyx.org>
2026-02-28 13:30:29 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:30:29 up  1:52,  4 users,  load average: 0.10, 0.04, 0.01

---
+++ npc/encoder/csrc/main.cpp
@@ -0,0 +1,18 @@
+#include <nvboard>
+#include <Vtop>
+
+static Vtop dut;
+
+static void nvboard_bind_all_pins(Vtop *top);
+
+int main()
+{
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+	
+	while (1)
+	{
+		nvboard_update();
+		dut.eval();
+	}
+}	
