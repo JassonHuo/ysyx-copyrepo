38751baf36cee05e4562e2ef045e3d887ef88dc6
tracer-ysyx <tracer@ysyx.org>
2026-03-02 20:18:34 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:18:34 up  5:33,  2 users,  load average: 0.53, 0.26, 0.17

--- npc/NJU/encoder/vsrc/bcd7seg.v
+++ npc/NJU/encoder/vsrc/bcd7seg.v
@@ -8,7 +8,11 @@ module bcd7seg(
 	  0: seg = 7'b0000001;
 	  1: seg = 7'b1001111;
 	  2: seg = 7'b0010010;
-	  3: seg = 7'b0110000;
+	  3: seg = 7'b0000110;
+	  4: seg = 7'b1001100;
+	  5: seg = 7'b0100100;
+	  6: seg = 7'b0100000;
+	  7: seg = 7'b0001111;
 	  default: seg = 7'b0000001;
 	endcase
   end
