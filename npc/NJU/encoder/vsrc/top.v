b01493e280f227cfc70bac726de9b074abb58bd3
tracer-ysyx <tracer@ysyx.org>
2026-03-02 19:29:18 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:29:18 up  4:43,  2 users,  load average: 0.53, 0.14, 0.05

--- npc/NJU/encoder/vsrc/top.v
+++ npc/NJU/encoder/vsrc/top.v
@@ -2,15 +2,51 @@ module top(
   input [7: 0] in,
   output reg [2: 0] out,
   output no_zero,
-  input en
+  input en,
+  output reg [7: 0] seg
 );
  
   assign no_zero = (in != 8'b0);
-  integer i;
   always@(*)begin
 	if(en)begin
-		out = 3'b0;
-		for (i = 0; i < 8; i = i + 1) if(in[i] == 1) out = i[2: 0];
+	  casez(in)
+		8'b1zzzzzzz: begin
+		  out = 7;
+		  seg = 8'b11100000;
+		end
+		8'b01zzzzzz: begin
+		  out = 6;
+		  seg = 8'b10111110;
+		end
+		8'b001zzzzz: begin
+		  out = 5;
+		  seg = 8'b10110110;
+		end
+		8'b0001zzzz: begin
+		  out = 4;
+		  seg = 8'b01100110;
+		end
+		8'b00001zzz: begin
+		  out = 3;
+		  seg = 8'b11110010;
+		end
+		8'b000001zz: begin
+		  out = 2;
+		  seg = 8'b11011010;
+		end
+		8'b0000001z: begin
+		  out = 1;
+		  seg = 8'b01100000;
+		end
+		8'b00000001: begin
+		  out = 0;
+		  seg = 8'b11111100;
+		end
+		default: begin
+		  out = 0;
+		  seg = 8'b11111100;
+		end
+	  endcase
 	end
 	else out = 0;
   end
