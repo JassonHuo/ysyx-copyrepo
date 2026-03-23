98583fe4d097b8390632b030d4092f9864f58058
tracer-ysyx <tracer@ysyx.org>
2026-03-23 22:41:49 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:41:49 up  8:58,  7 users,  load average: 0.65, 0.37, 0.40

--- npc/NJU/ps2_keyboard/vsrc/top.v
+++ npc/NJU/ps2_keyboard/vsrc/top.v
@@ -51,7 +51,7 @@ module top(
   );
 
   bcd bcd2(
-	.data(counter[15: 8]),
+	.data({counter[8], counter[9], counter[10], counter[11], counter[12], counter[13], counter[14], counter[15]}),
 	.clk(clk),
 	.bcd_low(seg4),
 	.bcd_high(seg5),
@@ -59,7 +59,7 @@ module top(
   );
 
   bcd bcd3(
-	.data(counter[7: 0]),
+	.data({counter[7], counter[6], counter[5], counter[4], counter[3], counter[2], counter[1], counter[0]}),
 	.clk(clk),
 	.bcd_low(seg6),
 	.bcd_high(seg7),
