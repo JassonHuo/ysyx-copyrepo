c96a702d23ff31bbf04ce763e8c0a5c52c38795a
tracer-ysyx <tracer@ysyx.org>
2026-03-29 19:38:47 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:38:47 up  4:34,  0 users,  load average: 0.55, 0.32, 0.30

--- npc/NJU/ps2_keyboard/top.v
+++ npc/NJU/ps2_keyboard/top.v
@@ -23,65 +23,43 @@ module top(
   reg [7: 0] use_data;
   reg down;
 
-  always@(*)begin
-	case(state)
-	  NONE: next_state = (data_in == 8'b0) ? NONE: DOWN;
-	  DOWN: next_state = (data_in == 8'hF0) ? WAIT: DOWN;
-	  WAIT: next_state = (ready && ~nextdata_n) ? WAIT: NONE;
-	  default: next_state = NONE;
-    endcase
-  end
-
   always@(posedge clk)begin
-//	if(state != next_state)
-	  $display("%x", data_in);
-	if(~rstn)begin
-	  state <= NONE;
-	end
-	else begin
-	  state <= next_state;
-	end
-  end
-
-  always@(posedge clk)begin
-//	if(state != next_state)
-//	  $display("%d", data_in);
-	if(~rstn)begin
-	  nextdata_n <= 1;
-	  data_in <= 8'b0;
-	end	 
-	else begin
-	  if(ready && nextdata_n)begin
-		nextdata_n <= 1'b0;
-		data_in <= data_kbd;
-	  end
-	  else if(!ready)begin
+	$display("%d", state);
+	  if(!rstn)begin
+		state <= NONE;
 		nextdata_n <= 1'b1;
-		if(state == WAIT && next_state == NONE)
-		  data_in <= 8'b0;
-		else
-		  data_in <= data_in;
+		down <= 1;
 	  end
-	end	  
+	  else if(ready && !nextdata_n)begin
+		nextdata_n <= 1'b0;
+		down <= 1;
+		use_data <= use_data;
+		case(state)
+		  NONE:begin
+			if(data_in == 8'b0)
+			  state <= NONE;
+			else
+			  state <= DOWN;
+		  end
+		  DOWN:begin
+			down <= 0;
+			use_data <= data_in;
+			if(data_in == 8'hF0)
+			  state <= WAIT;
+			else
+			  state <= DOWN;
+		  end
+		  WAIT:begin
+			if(data_in == 8'b0)
+			  state <= WAIT;
+			else
+			  state <= NONE;
+		  end
+		endcase
+	  end
+	  else if(nextdata_n)
+		nextdata_n <= 1'b0;
   end
-
-  always@(*)begin
-	case(state)
-	  NONE:begin
-		down = 1;
-	  end
-	  DOWN:begin
-		down = 0;
-	  end
-	  WAIT:begin
-		down = 1;
-	  end
-	  default:
-		down = 1;
-	endcase
-  end
-
-assign use_data = data_in;  
   
   ps2_keyboard kbd0(
 	.clk(clk),
--- npc/NJU/ps2_keyboard/vsrc/top.v
+++ npc/NJU/ps2_keyboard/vsrc/top.v
@@ -16,68 +16,75 @@ module top(
 
   parameter NONE = 0, DOWN = 1, WAIT = 2;
 
-  reg [2: 0] state, next_state;
+  reg [1:0] state;
   wire ready;
   reg nextdata_n;
-  reg [7: 0] data_in;
-  reg [7: 0] use_data;
+  wire [7:0] data_in;   // wire，接 ps2_keyboard 的 assign 输出
+  reg [7:0] use_data;
   reg down;
+  reg prev_ready;       // 上一拍的 ready，用于检测上升沿
 
-  always@(posedge clk)begin
-	$display("%d", state);
-	  if(!rstn)begin
-		state <= NONE;
-		nextdata_n <= 1'b1;
-		down <= 1;
-	  end
-	  else if(ready && !nextdata_n)begin
-		nextdata_n <= 1'b0;
-		down <= 1;
-		use_data <= use_data;
-		case(state)
-		  NONE:begin
-			if(data_in == 8'b0)
-			  state <= NONE;
-			else
-			  state <= DOWN;
-		  end
-		  DOWN:begin
-			down <= 0;
-			use_data <= data_in;
-			if(data_in == 8'hF0)
-			  state <= WAIT;
-			else
-			  state <= DOWN;
-		  end
-		  WAIT:begin
-			if(data_in == 8'b0)
-			  state <= WAIT;
-			else
-			  state <= NONE;
-		  end
-		endcase
-	  end
-	  else if(nextdata_n)
-		nextdata_n <= 1'b0;
+  always @(posedge clk) begin
+    if (!rstn) begin
+      state      <= NONE;
+      nextdata_n <= 1'b1;
+      down       <= 1'b1;
+      use_data   <= 8'b0;
+      prev_ready <= 1'b0;
+    end else begin
+      // 默认值
+      nextdata_n <= 1'b1;
+      down       <= 1'b1;
+      prev_ready <= ready;
+
+      // 只在 ready 上升沿处理，保证每条数据只消费一次
+      if (ready && !prev_ready) begin
+        nextdata_n <= 1'b0;   // 单拍脉冲，通知 FIFO 移到下一条
+
+        case (state)
+          NONE: begin
+            // ready 为高说明 FIFO 有数据，直接跳 DOWN
+            state <= DOWN;
+          end
+
+          DOWN: begin
+            down     <= 1'b0;
+            use_data <= data_in;
+            if (data_in == 8'hF0)
+              state <= WAIT;
+            else
+              state <= DOWN;
+          end
+
+          WAIT: begin
+            // F0 后面那个字节是 break code，消耗掉，回 NONE
+            state <= NONE;
+          end
+        endcase
+      end
+    end
   end
-  
+
   ps2_keyboard kbd0(
-	.clk(clk),
-	.clrn(rstn),
-	.ps2_clk(ps2_clk),
-	.ps2_data(ps2_data),
-	.nextdata_n(nextdata_n),
-	.data(data_in),
-	.ready(ready),
-	.overflow(overflow)
+    .clk(clk),
+    .clrn(rstn),
+    .ps2_clk(ps2_clk),
+    .ps2_data(ps2_data),
+    .nextdata_n(nextdata_n),
+    .data(data_in),
+    .ready(ready),
+    .overflow(overflow)
   );
 
   bcd bcd0(
-	.data({use_data[0], use_data[1], use_data[2], use_data[3], use_data[4], use_data[5], use_data[6], use_data[7]}),
-	.clk(clk),
-	.down(down),
-	.bcd_low(seg0),
-	.bcd_high(seg1)
+    .data({data_in[0], data_in[1], data_in[2],
+           data_in[3], data_in[4], data_in[5],
+           data_in[6], data_in[7]}),
+    .clk(clk),
+    .down(down),
+    .bcd_low(seg0),
+    .bcd_high(seg1)
   );
 
 endmodule
+
