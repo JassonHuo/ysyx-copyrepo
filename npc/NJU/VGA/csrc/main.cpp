f10731fadd9437b803062a9b45327aab49616aea
tracer-ysyx <tracer@ysyx.org>
2026-04-01 21:22:53 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:22:53 up  7:21,  0 users,  load average: 0.48, 0.37, 0.25

--- npc/NJU/VGA/csrc/main.cpp
+++ npc/NJU/VGA/csrc/main.cpp
@@ -40,11 +40,7 @@ int main(int argc, char **argv)
 	extern void vga_update();
 	vga_update();
 	nvboard_update();
-	if(cnt == 0)
-	{
-	  one_cycle();
-	}
-	  cnt = (cnt == 100000 ? 0: cnt ++);
+	one_cycle();
 	if(dut->valid_out == 1)
 	{
 //	  printf("%d", dut->black);
