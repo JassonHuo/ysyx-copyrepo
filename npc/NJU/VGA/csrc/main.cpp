c04ee41b346db93c1250ca0d297b6c954437396b
tracer-ysyx <tracer@ysyx.org>
2026-04-01 15:45:38 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:45:38 up  1:43,  0 users,  load average: 0.52, 0.15, 0.08

--- npc/NJU/VGA/csrc/main.cpp
+++ npc/NJU/VGA/csrc/main.cpp
@@ -2,36 +2,51 @@
 #include <Vtop.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <verilated.h>
+#include <verilated_fst_c.h>
 
-static TOP_NAME dut;
+VerilatedContext *contextp = new VerilatedContext;
+static TOP_NAME *dut = new TOP_NAME{contextp};
+VerilatedFstC * tfp = new VerilatedFstC;
 
 void nvboard_bind_all_pins(TOP_NAME* top);
 
 void one_cycle()
 {
-  dut.clk = 0;
-  dut.eval();
-  dut.clk = 1;
-  dut.eval();
+  dut->clk = 0;
+  dut->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+  dut->clk = 1;
+  dut->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
 }
 
-int main()
+int main(int argc, char **argv)
 {
   nvboard_init();
-  nvboard_bind_all_pins(&dut);
-  dut.rst = 0;
+  nvboard_bind_all_pins(dut);
+  dut->rst = 0;
   int cnt = 0;
   int cotl = 0;
 
-  while(1)
+  contextp->commandArgs(argc, argv);
+  contextp->traceEverOn(true);
+  contextp->trace(tfp, 99);
+  tfp->open("wave->fst");
+  int cycle = 500000;
+
+  while(cycle)
   {
 	nvboard_update();
 	cotl = (cotl >= 10000 ? 0: cotl + 1);
 	if(cotl == 0)
 	{
+	  cycle --;
 	one_cycle();
-	if(dut.valid_out == 1)
-	printf("pixel[%d], valid[%d]: r = %d, g = %d, b = %d, hsync = %d, vsynv = %d\n", cnt, dut.valid_out, dut.vga_r, dut.vga_g, dut.vga_b, dut.hsync, dut.vsync);
+	if(dut->valid_out == 1)
+	printf("pixel[%d], valid[%d]: r = %d, g = %d, b = %d, hsync = %d, vsynv = %d\n", cnt, dut->valid_out, dut->vga_r, dut->vga_g, dut->vga_b, dut->hsync, dut->vsync);
 	cnt ++;
 	}
   }
