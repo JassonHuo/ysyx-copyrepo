e259d91289c75825233d511cedf9f76f35346ea8
tracer-ysyx <tracer@ysyx.org>
2026-04-08 11:09:00 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:09:00 up 11 min,  0 users,  load average: 0.01, 0.01, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -25,6 +25,7 @@ void init_regex();
 void init_wp_pool();
 void execute();
 void isa_reg_display();
+word_t pmem_read();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
@@ -57,7 +58,7 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 static int cmd_si(char *args);
 static int cmd_info(char *args);
-//static int cmd_x(char *args);
+static int cmd_x(char *args);
 
 static struct {
   const char *name;
@@ -69,7 +70,7 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
   { "si", "Execute by steps you want", cmd_si },
   { "info", "Display the status of register", cmd_info },
-//  { "x", "Scanf the memory", cmd_x },
+  { "x", "Scanf the memory", cmd_x },
 
   /* TODO: Add more commands */
 
@@ -126,7 +127,15 @@ static int cmd_info(char *args)
   else return 0;
 }
 
-//static int cmd_x(char *args)
+static int cmd_x(char *args)
+{
+  if(!args) return 0;
+  int n = atoi(strtok(NULL, " "));
+  uint32_t beg_addr;
+  sscanf(strtok(NULL, " "), "%x", &beg_addr);
+  printf("%d, %d", n, beg_addr);
+  return 0;
+}
 
 void sdb_set_batch_mode() {
   is_batch_mode = true;
