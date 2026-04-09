907d77cb92d9899ffea96be3d43e74462e7de33d
tracer-ysyx <tracer@ysyx.org>
2026-04-09 18:11:16 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:11:16 up  3:03,  0 users,  load average: 0.04, 0.01, 0.00

--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -68,9 +68,10 @@ void init_disasm() {
 }
 
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
-  printf("str: %s, size: %d, pc: %lx, code: %x, nbyte: %d\n", str, size, pc, *code, nbyte);
+//  printf("str: %s, size: %d, pc: %lx, code: %x, nbyte: %d\n", str, size, pc, *code, nbyte);
 	cs_insn *insn;
 	size_t count = cs_disasm_dl(handle, code, nbyte, pc, 0, &insn);
+//	printf("insn: %s\n", insn);
   assert(count == 1);
   int ret = snprintf(str, size, "%s", insn->mnemonic);
   if (insn->op_str[0] != '\0') {
