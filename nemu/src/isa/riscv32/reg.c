9b0f6342be1144d96ba4e118c2d28e86d6415814
tracer-ysyx <tracer@ysyx.org>
2026-04-09 18:29:00 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:29:00 up  3:21,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,7 +31,7 @@ void isa_reg_display() {
 	if(regs[i] == 0)
 	  printf("gpr[%d]\t%3s: \t0x%08x| %012d\n", i, regs[i], cpu.gpr[i], cpu.gpr[i]);
 	else
-	  printf("gpr[%d}\t%3s: \t\033[34m0x%08x\033[0m| \033[34m%012d[0m\n", i, regs[i], cpu.gpr[i], cpu.gpr[i]);
+	  printf("gpr[%d]\t%3s: \t\033[34m0x%08x\033[0m| \033[34m%012d\033[0m\n", i, regs[i], cpu.gpr[i], cpu.gpr[i]);
   }
 }
 
