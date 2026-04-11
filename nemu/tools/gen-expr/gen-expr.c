047ad272bea5875e9f888d67d3854942cb07a6b6
tracer-ysyx <tracer@ysyx.org>
2026-04-11 16:05:39 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:05:39 up  4:11,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -31,8 +31,14 @@ static char *code_format =
 "  return 0; "
 "}";
 
+static int buf_top = 0;
 static void gen_rand_expr() {
-  buf[0] = '\0';
+//  buf[0] = '\0';
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
