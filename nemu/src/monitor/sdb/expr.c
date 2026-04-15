c28653e0a9e1581430127f1f5d12b269e9e52fb7
tracer-ysyx <tracer@ysyx.org>
2026-04-15 18:34:44 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:34:44 up 1 day,  3:40,  0 users,  load average: 0.09, 0.07, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -257,7 +257,7 @@ static uint32_t eval(int p, int q) {
 	else if(tokens[p].type == TK_REG)
 	{
 	  bool success;
-	  num = isa_reg_str2val(tokens[p].str, &success);
+	  num = isa_reg_str2val(tokens[p].str + 1, &success);
 	  Assert(success, "%s %s %d: GPR Name Error", __FILE__, __func__, __LINE__);
 	  return num;
 	}
