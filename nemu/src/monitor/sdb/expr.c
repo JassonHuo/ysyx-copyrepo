f7afa38534baa742e5e3c09968c4c82cc5a4ae90
tracer-ysyx <tracer@ysyx.org>
2026-04-15 15:02:43 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:02:43 up 1 day, 8 min,  0 users,  load average: 0.01, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -296,7 +296,7 @@ static uint32_t eval(int p, int q) {
       case '-': return val1 - val2;
       case '*': return val1 * val2; 
       case '/':	
-				if(val2 == 0)
+				if(val2 != 0)
 				  return val1 / val2; 
 				else
 				{
