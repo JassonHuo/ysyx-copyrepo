6cbf4a1941313efcdfb90f91d00037fadcf3cd9b
tracer-ysyx <tracer@ysyx.org>
2026-05-02 18:25:18 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:25:18 up  5:27,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -22,22 +22,41 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  uint32_t point_addr;
+  struct watchpoint *prev;
 } WP;
 
 static WP wp_pool[NR_WP] = {};
-static WP *head = NULL, *free_ = NULL;
+static WP *head = NULL, *free_ = NULL, *tail = NULL;
 
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+	wp_pool[i].prev = (i == 0 ? NULL: &wp_pool[i - 1]);
   }
 
   head = NULL;
+  tail = head;
   free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(uint32_t addr)
+{
+  Assert(free_ != NULL,  "%s %s %d Memory Error: Watch Point Pool Overflow", __FILE__, __func__, __LINE__);
+  WP * tmp = free_;
+  free_ = free_->next;
+  tail->next = tmp;
+  tmp->prev = tail;
+  tmp->next = NULL;
+  tail = tmp;
+  tmp->point_addr = addr;
+  return tail;
+}
 
+void free_wp(WP* wp, bool* success)
+{
+  Assert(head != NULL, "%s %s %d Memory Error: Watch Point Pool Overflow", __FILE__, __func__, __LINE__);
+}
