4a680a210b7c167f8f3d2372dbd551295b376a46
tracer-ysyx <tracer@ysyx.org>
2026-05-04 14:03:44 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:03:44 up 16:48,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -304,9 +304,9 @@ bool div_by_zero = false;
 
 static uint32_t eval(int p, int q) {
   //printf("p: %d, q: %d\n", p, q);
-  for (int i = p; i <= q; i ++)
+//  for (int i = p; i <= q; i ++)
 //	printf("%s ", tokens[i].str);
-  printf("\n");
+//  printf("\n");
   //printf("%d, %d\n", p, q);
   if (p > q) {
 //	printf("Error in %s %d, function: %s\n", __FILE__, __LINE__, __func__);
