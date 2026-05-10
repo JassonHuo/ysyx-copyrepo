6e9da9b72ce0c1c6df542a59d2953b76c0e1d837
tracer-ysyx <tracer@ysyx.org>
2026-05-10 14:29:35 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:29:35 up  7:56,  0 users,  load average: 0.36, 0.18, 0.18

--- npc/NJU/ps2_keyboard/top.v
+++ npc/NJU/ps2_keyboard/top.v
@@ -1,136 +1,69 @@
 module top(
+  input rst,
   input clk,
-  input ps2_clk,
   input ps2_data,
-  input rstn,
+  input ps2_clk,
   output overflow,
-  output [6: 0] seg0,
-  output [6: 0] seg1,
-  output [6: 0] seg2,
-  output [6: 0] seg3,
-  output [6: 0] seg4,
-  output [6: 0] seg5,
-  output [6: 0] seg6,
-  output [6: 0] seg7
+  output reg [6: 0] seg0, seg1, seg2, seg3,
+  output reg [6: 0] seg4, seg5, seg6, seg7
 );
 
-  parameter NONE = 0, DOWN = 1, WAIT = 2;
-
-  reg [2: 0] state, next_state;
-  wire ready;
   reg nextdata_n;
   wire [7: 0] data_kbd;
-//  reg [7: 0] use_data;
-  reg down;
-  reg [7: 0] data_in;
-  reg ready_prev;
-  reg [15: 0] counter;
+  wire ready;
+  parameter NONE = 0, PRESS = 1, WAIT = 2;
+  reg [1: 0] state, next_state; 
+  reg [7: 0] data; 
+  reg reading;
 
-  always@(posedge clk)begin
-	  $display("%d", state);
-	  if(!rstn)begin
-		state <= NONE;
-		nextdata_n <= 1'b1;
-		down <= 1;
-	  end
-	  else begin
-		ready_prev <= ready;
-		nextdata_n <= 1'b1;
-		if(ready)begin
-		  data_in <= data_kbd;
-		  nextdata_n <= 1'b0;
-		  down <= 1'b0;
-		  case(state)
-			NONE:begin
-			  if(data_kbd == 8'b0)begin
-				state <= NONE;
-				down <= 1'b1;
-			  end
-			  else if(data_kbd == 8'hF0)begin
-				state <= WAIT;
-				down <= 1'b1;
-			  end
-			  else begin
-				state <= DOWN;
-				down <= 1'b0;
-			  end
-			end
-			DOWN:begin
-			  down <= 0;
-			  if(data_kbd == 8'hF0)begin
-				state <= WAIT;
-				down <= 1'b1;
-			  end
-			  else begin
-//				if(data_in != data_kbd) counter <= counter + 1;
-				counter <= counter + 1;
-				state <= DOWN;
-				down <= 1'b0;
-			  end
-			end
-			WAIT:begin
-			  if(data_kbd == 8'b0)begin
-				state <= WAIT;
-				down <= 1'b1;
-			  end
-			  else begin
-				state <= NONE;
-				down <= 1'b1;
-			  end
-			end
-			default:begin
-			  state <= NONE;
-			  down <= 1'b1;
-			end
-		  endcase
-		end
-		else begin 
-		  nextdata_n <= 1'b1;
-		end
-	  end
-/*		else if(!ready)begin
-		  used <= 1'b0;
-		  nextdata_n <= 1'b1;
-		end
-		else begin
-		  nextdata_n <= 1'b1;
-		end
-		*/
-	  end
-  
-  ps2_keyboard kbd0(
+  ps2_keyboard kbd(
 	.clk(clk),
-	.clrn(rstn),
+	.clrn(~rst),
 	.ps2_clk(ps2_clk),
 	.ps2_data(ps2_data),
 	.nextdata_n(nextdata_n),
 	.data(data_kbd),
 	.ready(ready),
 	.overflow(overflow)
-  );
+  );	
 
-  bcd bcd0(
-	.data(data_in),
-	.clk(clk),
-	.down(down),
-	.bcd_low(seg0),
-	.bcd_high(seg1)
-  );
+  always@(*)begin
+	if(rst)
+	  next_state = NONE;
+	else begin
+	  case(state)
+		NONE: next_state = (data == 8'b0 ? NONE: (data == 8'hF0 ? WAIT: PRESS));
+		PRESS: next_state = (data == 8'hF0 ? WAIT: PRESS);
+		WAIT: next_state = (data == 8'b0 || data == 8'hF0 ? WAIT: NONE);
+		default: next_state = NONE;
+	  endcase
+	end
+  end
 
-  bcd bcd1(
-	.data(counter[7: 0]),
-	.clk(clk),
-	.down(1'b0),
-	.bcd_low(seg4),
-	.bcd_high(seg5)
-  );
-  
-  bcd bcd2(
-	.data(counter[15: 8]),
-	.clk(clk),
-	.down(1'b0),
-	.bcd_low(seg6),
-	.bcd_high(seg7)
-  );
+  always@(posedge clk)begin
+	nextdata_n <= 1'b1;
+	$display(state);
+//	$display("%h", data);
+	if(rst)begin
+	  state <= NONE;
+	  data <= 8'b0;
+	  reading <= 1'b1;
+	end
+	else begin
+	  if(nextdata_n)
+		reading <= 1'b0;
+	  state <= next_state;
+	  reading <= 1'b1;
+	  if(ready) begin
+		if(~reading)
+		  data <= data_kbd;
+		else
+		  nextdata_n <= 1'b0;
+	  end
+	  else begin
+//		data <= 8'b0;
+	  end
+	end
+  end
 
 endmodule
