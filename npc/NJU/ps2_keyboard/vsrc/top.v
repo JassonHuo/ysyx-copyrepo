938d735440f84bada56a64533e9106aafce49c1b
tracer-ysyx <tracer@ysyx.org>
2026-05-10 13:43:04 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:43:04 up  7:09,  0 users,  load average: 1.08, 0.48, 0.22

--- npc/NJU/ps2_keyboard/vsrc/top.v
+++ npc/NJU/ps2_keyboard/vsrc/top.v
@@ -30,9 +30,9 @@ module top(
 	  next_state = NONE;
 	else begin
 	  case(state)
-		NONE: next_state = (data == 8'b0 ? NONE: PRESS);
+		NONE: next_state = (data == 8'b0 ? NONE: (data == 8'hF0 ? WAIT: PRESS));
 		PRESS: next_state = (data == 8'hF0 ? PRESS: WAIT);
-		WAIT: next_state = (data == 8'b0 ? WAIT: NONE);
+		WAIT: next_state = (data == 8'b0 && data != 8'hF0 ? WAIT: NONE);
 		default: next_state = NONE;
 	  endcase
 	end
