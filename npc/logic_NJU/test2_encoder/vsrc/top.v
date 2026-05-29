c103605e4f2d145e21bbe56b5b80961527382e7c
tracer-ysyx <tracer@ysyx.org>
2026-05-29 14:05:39 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:05:39 up 50 min,  0 users,  load average: 0.48, 0.21, 0.10

--- npc/logic_NJU/test2_encoder/vsrc/top.v
+++ npc/logic_NJU/test2_encoder/vsrc/top.v
@@ -1,16 +1,22 @@
 module top(
   input [7: 0] in,
-  output reg [2: 0] out
+  input en,
+  output reg [2: 0] out,
+  output reg flag
 );
 
 
   integer i;
   always@(*)begin
 	out = 3'b0;
-	for(i = 7; i >= 0; i = i - 1)begin
-	  if(in[i] == 1)begin
-		out = i[2: 0];
-		break;
+	flag = 1'b0;
+	if(en == 1)begin
+	  for(i = 7; i >= 0; i = i - 1)begin
+		if(in[i] == 1)begin
+		  out = i[2: 0];
+		  flag = 1;
+		  break;
+		end
 	  end
 	end
   end
