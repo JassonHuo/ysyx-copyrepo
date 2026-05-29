6cfcf84817da282515e3612d7cb3432d1a66aeda
tracer-ysyx <tracer@ysyx.org>
2026-05-29 14:17:36 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:17:36 up  1:02,  0 users,  load average: 0.50, 0.19, 0.12

--- npc/logic_NJU/test2_encoder/vsrc/seg.v
+++ npc/logic_NJU/test2_encoder/vsrc/seg.v
@@ -5,8 +5,14 @@ module seg(
 
   always@(*)begin
 	case(data)
-	  3'b0: seg = 7'b1111111;
-	  default: seg = 7'b0000000;
+	  3'h0: seg = 7'b0000001;
+	  3'h1: seg = 7'b1001111;
+	  3'h2: seg = 7'b0010010;
+	  3'h3: seg = 7'b0000110;
+	  3'h4: seg = 7'b1001100;
+	  3'h5: seg = 7'b0100100;
+	  3'h6: seg = 7'b0001111;
+	  default: seg = 7'b0000001;
 	endcase
   end
 
