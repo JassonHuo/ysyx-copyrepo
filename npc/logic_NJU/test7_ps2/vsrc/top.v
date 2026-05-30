698afc64a645b319e750abe02cab2d413cc802db
tracer-ysyx <tracer@ysyx.org>
2026-05-30 13:30:39 +0800

    > sim RTL ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:30:39 up 1 day, 15 min,  0 users,  load average: 1.15, 0.29, 0.10

---
+++ npc/logic_NJU/test7_ps2/vsrc/top.v
@@ -0,0 +1,52 @@
+module top(
+  input clk,
+  input clr,
+  input ps2_clk,
+  input ps2_data,
+  output [6: 0] seg0,
+  output [6: 0] seg1,
+  output [6: 0] seg2,
+  output [6: 0] seg3,
+  output overflow
+);
+
+  wire [7: 0] data_kbd;
+  wire ready;
+  wire nextdata_n;
+  parameter NONE = 0, PRESS = 1, WAIT = 2;
+  reg [1: 0] state, next_state;
+
+  ps2_keyboard my_kbd(
+	.clk(clk),
+	.clrn(~clr),
+	.ps2_clk(ps2_clk),
+	.ps2_data(ps2_data),
+	.data(data_kbd),
+	.ready(ready),
+	.nextdata_n(nextdata_n),
+	.overflow(overflow)
+  );
+
+  wire [7: 0] data = data_kbd & {8{ready}};
+
+  always@(*)begin
+	if(clr) next_state = NONE;
+	else begin
+	  case(state)
+		NONE: next_state = (data == 0 ? NONE: PRESS);
+		PRESS: next_state = (data == 8'hF0 ? WAIT: PRESS);
+		WAIT: next_state = (data == 8'h0 ? WAIT: NONE);
+	  endcase
+	end
+  end
+
+  always@(posedge clk)begin
+	$display(state);
+	if(clr)
+	  state <= NONE;
+	else begin
+	  state <= next_state;
+	end
+  end
+
+endmodule
