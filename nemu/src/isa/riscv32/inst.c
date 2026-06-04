77b77d2fc7c955eca6f801034c136404ae891c1e
tracer-ysyx <tracer@ysyx.org>
2026-06-04 17:33:39 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:33:39 up 1 day,  4:06,  0 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -61,6 +61,7 @@ static int decode_exec(Decode *s) {
   word_t src1 = 0, src2 = 0, imm = 0; \
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
+  printf("%8x\n", imm);\
 }
 
   INSTPAT_START();
