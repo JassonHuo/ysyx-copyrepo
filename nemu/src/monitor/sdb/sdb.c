a490e109c18169a6542b41803d1ff154ccad7e99
tracer-ysyx <tracer@ysyx.org>
2026-06-06 13:31:15 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:31:15 up  3:58,  0 users,  load average: 0.00, 0.05, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -61,7 +61,10 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
-  return -1;
+//  return -1;
+  nemu_state.state = NEMU_QUIT;
+  return 0;
+
 }
 
 static int cmd_help(char *args);
