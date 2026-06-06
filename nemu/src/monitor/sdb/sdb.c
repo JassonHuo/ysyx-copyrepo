dc618901bbce8b21ecc63d54bc2baee8885a8b05
tracer-ysyx <tracer@ysyx.org>
2026-06-06 13:45:00 +0800

    > compile NEMU ysyx_22040000 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:45:00 up  4:11,  0 users,  load average: 0.08, 0.02, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -236,12 +236,16 @@ void sdb_mainloop() {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
+		/*
         if (nemu_state.state == NEMU_QUIT) { 
 		  return; 
 		}
+		*/
         break;
       }
     }
+	if(nemu_state.state == NEMU_QUIT)
+	  return;
 
     if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
   }
