e44779604012bb1eeb4f3ce5d322ca93fc322010
tracer-ysyx <tracer@ysyx.org>
2026-06-30 13:55:54 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:55:54 up 58 min,  0 users,  load average: 0.54, 0.23, 0.09

--- npc/logical_test/logical_test/csrc/main.cpp
+++ npc/logical_test/logical_test/csrc/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char**argv)
   nvboard_init();
   while(!contextp->gotFinish())
   {
-	uint32_t counter = 100000000;
+	uint64_t counter = 100000000000;
 	while(counter && !contextp->gotFinish())
 	{
 	  counter --;
