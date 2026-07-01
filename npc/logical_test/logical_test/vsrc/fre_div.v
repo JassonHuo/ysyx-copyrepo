d8d55fa8a0cccdb1c0b4b5ea021921ea893d51f3
tracer-ysyx <tracer@ysyx.org>
2026-07-01 10:04:54 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:04:54 up  1:28,  0 users,  load average: 0.76, 0.58, 0.35

--- npc/logical_test/logical_test/vsrc/fre_div.v
+++ npc/logical_test/logical_test/vsrc/fre_div.v
@@ -5,7 +5,7 @@ module fre_div(
 
   reg [63: 0] counter;
   always@(posedge clk)begin
-	  counter <= (counter == 0 ? 64'd60000000: counter - 1);
+	  counter <= (counter == 0 ? 64'd50000000: counter - 1);
 	  clk_out <= (counter != 0 ? clk_out: ~clk_out);
   end
 
