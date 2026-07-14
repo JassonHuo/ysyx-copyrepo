5df7a02b7154107f31d9ead719d2a8e2cce4a74d
tracer-ysyx <tracer@ysyx.org>
2026-07-14 19:34:50 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:34:50 up 13:34,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,13 +1,13 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
 
+uint64_t start_time;
 void __am_timer_init() {
+  start_time = (((uint64_t)inl(RTC_ADDR + 4) << 32) | inl(RTC_ADDR));
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = (((uint64_t)inl(RTC_ADDR + 4) << 32) | inl(RTC_ADDR));
-  printf("%ld", uptime->us);
+  uptime->us = (((uint64_t)inl(RTC_ADDR + 4) << 32) | inl(RTC_ADDR)) - start_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
