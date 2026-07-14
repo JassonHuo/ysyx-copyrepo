2aa8afa3920245c306aab6662eac18d58fa5e183
tracer-ysyx <tracer@ysyx.org>
2026-07-14 10:17:41 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:17:41 up  4:17,  0 users,  load average: 0.08, 0.02, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -161,10 +161,11 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
+	if (nemu_state.state == NEMU_ABORT)
+	  display_inst();
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
-  display_inst();
 }
 
 static void statistic() {
