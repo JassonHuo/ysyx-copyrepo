1d352635db7e3c7145873766276a153534c98bec
tracer-ysyx <tracer@ysyx.org>
2026-07-14 08:03:08 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:03:08 up  2:02,  0 users,  load average: 0.02, 0.01, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -91,11 +91,23 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #endif
 }
 
+static void inQueue()
+{
+  return;
+}
 static void manage_queue(Decode *s)
 {
   word_t pc = s->pc;
   uint8_t *inst = (uint8_t*)&s->isa.inst;
-  printf("%08x %02x %02x %02x %02x\n", pc, inst[0], inst[1], inst[2], inst[3]);
+  int ilen = s->snpc - s->pc;
+  char inst_str[32];
+  char *p = inst_str;
+  for(int i = ilen - 1; i >= 0; i --)
+  {
+	p += snprintf(p, 4, "%02x", inst[i]);
+  }
+  printf("%08x %s\n", pc, inst_str);
+  inQueue();
 }
 
 static void execute(uint64_t n) {
