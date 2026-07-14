a7fadaa71d35618bb6503f5c6a5d48337df62270
tracer-ysyx <tracer@ysyx.org>
2026-07-14 10:07:59 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:07:59 up  4:07,  0 users,  load average: 0.08, 0.02, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -150,7 +150,7 @@ static void display_inst()
 	disassemble(inst_dis, 50, pc_Queue[i], (uint8_t*)(code_Queue + i), ilen);
 	if(i == tail - 1)
 	  printf("  -->");
-	printf("\t0x%08x: \033[042%-30s\033[0%-5s\n", pc_Queue[i], inst_dis, Queue[i]);
+	printf("\t0x%08x: %-30s %-5s\n", pc_Queue[i], inst_dis, Queue[i]);
 	i = (i + 1) % qu_size;
   }
 }
