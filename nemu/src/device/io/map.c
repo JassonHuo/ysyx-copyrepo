ef59583794a16c8ed394734bc90b0152f088eb51
tracer-ysyx <tracer@ysyx.org>
2026-07-15 18:03:31 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:03:31 up  6:50,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -57,8 +57,8 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
-  printf("Read device %s\n", map->name);
   word_t ret = host_read(map->space + offset, len);
+  printf("%08x: Read data:  %5d form device: %s\n", cpu.pc, ret, map->name);
   return ret;
 }
 
@@ -68,5 +68,5 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
-  printf("Write device %s\n", map->name);
+  printf("%08x; Write data: %5d  to  device: %s\n", cpu.pc, data, map->name);
 }
