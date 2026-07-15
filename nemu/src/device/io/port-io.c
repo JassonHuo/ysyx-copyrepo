bcd64af17a9d8e769e7e11b7a947c453bb782be1
tracer-ysyx <tracer@ysyx.org>
2026-07-15 17:58:22 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:58:22 up  6:45,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/io/port-io.c
+++ nemu/src/device/io/port-io.c
@@ -34,13 +34,16 @@ void add_pio_map(const char *name, ioaddr_t addr, void *space, uint32_t len, io_
 }
 
 /* CPU interface */
+#include <isa.h>
 uint32_t pio_read(ioaddr_t addr, int len) {
   assert(addr + len - 1 < PORT_IO_SPACE_MAX);
   int mapid = find_mapid_by_addr(maps, nr_map, addr);
   assert(mapid != -1);
   printf("test1\n");
-  printf("Read device: %s\n", maps[mapid].name);
-  return map_read(addr, len, &maps[mapid]);
+  uint32_t read_data = map_read(addr, len, &maps[mapid]);
+  extern CPU_state cpu;
+  printf("%08x: Read data: %5d form device: %s\n", cpu.pc, read_data, maps[mapid].name);
+  return read_data;
 }
 
 void pio_write(ioaddr_t addr, int len, uint32_t data) {
@@ -48,6 +51,6 @@ void pio_write(ioaddr_t addr, int len, uint32_t data) {
   int mapid = find_mapid_by_addr(maps, nr_map, addr);
   assert(mapid != -1);
   printf("test2\n");
-  printf("Write device: %s\n", maps[mapid].name);
+  printf("Write data: %d to device: %s\n", data, maps[mapid].name);
   map_write(addr, len, data, &maps[mapid]);
 }
