1c84b1a12244d354d41a41e7777b66866ccfe089
tracer-ysyx <tracer@ysyx.org>
2026-07-18 21:29:45 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:29:45 up  2:01,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -53,7 +53,10 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
 #ifdef CONFIG_MTRACE
-  printf("read memory from addr 0x%08x to 0x%08x\n", (word_t)addr, (word_t)(addr + len - 1));
+  if(len == 1)
+	printf("read memory at addr 0x%08x\n", (word_t)addr);
+  else
+	printf("read memory from addr 0x%08x to 0x%08x\n", (word_t)addr, (word_t)(addr + len - 1));
 #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -63,7 +66,10 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
 #ifdef CONFIG_MTRACE
-  printf("write memory from addr	0x%08x to 0x%08x\n", (word_t)addr, (word_t)(addr + len - 1));
+  if(len == 1)
+	printf("write memory at addr 0x%08x\n", (word_t)addr);
+  else
+	printf("write memory from addr	0x%08x to 0x%08x\n", (word_t)addr, (word_t)(addr + len - 1));
 #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
