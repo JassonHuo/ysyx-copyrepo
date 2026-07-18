8317f3e2ba6489c5271ac9fc5696501997e6f46d
tracer-ysyx <tracer@ysyx.org>
2026-07-18 20:53:39 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:53:39 up  1:25,  0 users,  load average: 0.04, 0.01, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -48,7 +48,7 @@ void display_iring()
 	if(i != IRING_SIZE - 1 && iringbuf[i + 1] != 0)
 	  flag = 0;
 	}
-	printf("i = %d p = %d %s\n", i, iring_p, iringbuf[i]);
+	printf("%s\n", iringbuf[i]);
   }
 }
 
