32ef08854128ef8ba9bec36c224316c5f64b02ea
tracer-ysyx <tracer@ysyx.org>
2026-07-20 12:37:11 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:37:11 up 58 min,  0 users,  load average: 0.16, 0.04, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -166,7 +166,18 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
 }
 
-//static int layer = 0;
+#define FUNCTION_MATCH do{\
+	for(fun = 0; fun < fun_top; fun++)\
+	{\
+	  if(tar_addr >= functs[fun].addr && tar_addr < functs[fun].addr + functs[fun].size)\
+		break;\
+	}\
+	if(fun == fun_top)\
+	{\
+	  printf("Unknown function\n");\
+	  exit(1);\
+	} \
+}while(0)
 
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
@@ -209,38 +220,6 @@ static void exec_once(Decode *s, vaddr_t pc) {
   memcpy(iringbuf[iring_p], tmp, 100);
   iring_p = (iring_p + 1) % IRING_SIZE;
 
-  /*
-#ifdef CONFIG_FTRACE
-  uint8_t opcode = full_inst & 0x7f;
-  char fb_tmp[100] = {0};
-  uint8_t rd = (full_inst >> 12) & 0x1f;
-//  uint8_t rs1 = (full_inst >> 15) & 0x1f;
-  if(opcode == 0b1101111 && rd == 1)
-  {
-	uint32_t imm = (((int32_t)full_inst >> 30) << 20) | (((full_inst >> 12) & 0xff) << 12) | (((full_inst >> 20) & 0x1) << 11) | (((full_inst >> 21) & 0x3ff) << 1);
-	int fun = 0;
-	uint32_t tar_addr = imm + pc;
-	for(fun = 0; fun < fun_top; fun++)
-	{
-	  if(tar_addr >= functs[fun].addr && tar_addr < functs[fun].addr + functs[fun].size)
-		break;
-	}
-	if(fun == fun_top)
-	{
-	  printf("Unknown function\n");
-	  exit(1);
-	}
-	int p = 0;
-	p += sprintf(fb_tmp, "%08x:-", s->pc);
-	for(int i = 0; i < layer; i++) p += sprintf(fb_tmp + p, "--");
-	p += sprintf(fb_tmp + p, "call [%s@%08x]\n", functs[fun].func_name, functs[fun].addr);
-	layer ++;
-  }
-  printf("%s\n", fb_tmp);
-#endif
-*/
-
-//  p += snprintf(p, 2, "\n");
 #endif
 
 #ifdef CONFIG_FTRACE
@@ -248,12 +227,14 @@ static void exec_once(Decode *s, vaddr_t pc) {
   uint8_t opcode = full_inst & 0x7f;
   char fb_tmp[100] = {0};
   uint8_t rd = (full_inst >> 7) & 0x1f;
-//  uint8_t rs1 = (full_inst >> 15) & 0x1f;
+  uint8_t rs1 = (full_inst >> 15) & 0x1f;
+  int fun = 0;
   if(opcode == 0b1101111 && rd == 1)
   {
 	uint32_t imm = (((int32_t)full_inst >> 30) << 20) | (((full_inst >> 12) & 0xff) << 12) | (((full_inst >> 20) & 0x1) << 11) | (((full_inst >> 21) & 0x3ff) << 1);
-	int fun = 0;
 	uint32_t tar_addr = imm + pc;
+	/*
+	int fun = 0;
 	for(fun = 0; fun < fun_top; fun++)
 	{
 	  if(tar_addr >= functs[fun].addr && tar_addr < functs[fun].addr + functs[fun].size)
@@ -263,13 +244,38 @@ static void exec_once(Decode *s, vaddr_t pc) {
 	{
 	  printf("Unknown function\n");
 	  exit(1);
-	}
+	} 
+	*/
+	FUNCTION_MATCH;
 	int p = 0;
 	p += sprintf(fb_tmp, "%08x:-", s->pc);
 	for(int i = 0; i < layer; i++) p += sprintf(fb_tmp + p, "--");
-	p += sprintf(fb_tmp + p, "call [%s@%08x]\n", functs[fun].func_name, functs[fun].addr);
+	p += sprintf(fb_tmp + p, "call [%s@%08x]", functs[fun].func_name, functs[fun].addr);
 	layer ++;
-  printf("%s\n", fb_tmp);
+	fb_inQue(fb_tmp);
+	printf("%s\n", fb_tmp);
+  }
+  else if(opcode == 0b1100111 && rs1 == 1 && rd == 0)
+  {
+	uint32_t imm = ((int32_t)full_inst >> 20) & 0xfff;
+	if(imm == 0)
+	{
+	  layer--;
+	  bool success;
+	  uint32_t tar_addr = isa_reg_str2val("ra", &success);
+	  if(!success)
+	  {
+		printf("Wrong Register\n");
+		exit(1);
+	  }
+	  FUNCTION_MATCH;
+	  int p = 0;
+	  p += sprintf(fb_tmp, "%08x:-", s->pc);
+	  for(int i = 0; i < layer; i ++) p += sprintf(fb_tmp + p,  "--");
+	  p += sprintf(fb_tmp + p, "ret [%s]", functs[fun].func_name);
+	  fb_inQue(fb_tmp);
+	  printf("%s\n", fb_tmp);
+	}
   }
 #endif
 }
