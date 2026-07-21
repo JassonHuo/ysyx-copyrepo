f5314ada6e638062182397a55146ec482babf2ec
tracer-ysyx <tracer@ysyx.org>
2026-07-21 15:17:01 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:17:01 up 16:55,  0 users,  load average: 0.00, 0.00, 0.01

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,6 +18,8 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
+extern void (*ref_difftest_exec)(uint64_t n);
+
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if(direction == DIFFTEST_TO_DUT)
 	for(int i = 0; i < n; i ++)
@@ -30,6 +32,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
+  /*
   word_t *p = (word_t*)&cpu;
   printf("test\n");
   if(direction == DIFFTEST_TO_DUT)
@@ -52,10 +55,12 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
   }
   else
 	assert(0);
+	*/
+
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-
+  ref_difftest_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
