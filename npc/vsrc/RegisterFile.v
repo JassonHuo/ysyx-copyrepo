fcd555b06e660612e130697748151f81175e6df8
M_E662 <M_E662@noreply.gitcode.com>
2026-07-21 20:35:24 +0800

    通过dpi-c使c侧可以读取Verilog代码的寄存器数据

--- npc/vsrc/RegisterFile.v
+++ npc/vsrc/RegisterFile.v
@@ -16,4 +16,9 @@ module RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
   assign rdata1 = rf[raddr1];
   assign rdata2 = rf[raddr2];
   assign a0 = rf[10];
+  function int get_Reg(int idx);
+	return rf[idx];
+  endfunction
+
+  export "DPI-C" function get_Reg;
 endmodule
