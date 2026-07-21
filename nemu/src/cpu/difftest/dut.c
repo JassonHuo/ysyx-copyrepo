b72e0c729f4e3034ad79086c19e3ae842442e8bc
tracer-ysyx <tracer@ysyx.org>
2026-07-21 15:26:05 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:26:05 up 17:05,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -123,8 +123,10 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
 
   ref_difftest_exec(1);
-  printf("test\n");
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+
+  for(int i = 0; i < 16; i ++)
+	printf("%d ", *((word_t*)&ref_r + i));
 
   checkregs(&ref_r, pc);
 }
