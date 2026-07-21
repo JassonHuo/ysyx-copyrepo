ad5fa95361fe713f50f4d3a81df24929ee4f3d23
tracer-ysyx <tracer@ysyx.org>
2026-07-21 15:29:18 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:29:18 up 17:08,  0 users,  load average: 0.10, 0.03, 0.01

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,7 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if(ref_r->pc == pc)
+  if(ref_r->pc == cpu.pc)
 	return true;
   return false;
 }
