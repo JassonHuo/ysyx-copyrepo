57d658cc4206ccfeed327862ac60aeb89d3a3015
tracer-ysyx <tracer@ysyx.org>
2026-07-24 09:20:36 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:20:36 up 20 min,  0 users,  load average: 0.12, 0.06, 0.04

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,10 +18,10 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
-extern void (*ref_difftest_regcpy)(void *dut, bool direction);
-extern void (*ref_difftest_exec)(uint64_t n);
-extern void (*ref_difftest_raise_intr)(uint64_t NO);
+//extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
+//extern void (*ref_difftest_regcpy)(void *dut, bool direction);
+//extern void (*ref_difftest_exec)(uint64_t n);
+//extern void (*ref_difftest_raise_intr)(uint64_t NO);
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   /*
@@ -34,11 +34,11 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
   else
 	assert(0);
 	*/
-  ref_difftest_memcpy(addr, buf, n, direction);
+//  ref_difftest_memcpy(addr, buf, n, direction);
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  ref_difftest_regcpy(dut, direction);
+//  ref_difftest_regcpy(dut, direction);
   /*
   word_t *p = (word_t*)&cpu;
   printf("test\n");
@@ -67,11 +67,11 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  ref_difftest_exec(n);
+//  ref_difftest_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
-  ref_difftest_raise_intr(NO);
+//  ref_difftest_raise_intr(NO);
 }
 
 __EXPORT void difftest_init(int port) {
