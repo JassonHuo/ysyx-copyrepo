08cf8fd8456a771bc874a36a943576099e7e354c
tracer-ysyx <tracer@ysyx.org>
2026-07-24 12:11:54 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:11:54 up  3:11,  0 users,  load average: 0.52, 0.51, 0.23

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -24,13 +24,17 @@
 //extern void (*ref_difftest_raise_intr)(uint64_t NO);
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  printf("copy memory at %08x\n", addr);
   if(direction == DIFFTEST_TO_DUT)
 	for(int i = 0; i < n; i ++)
 	  *(char*)(buf + i) = paddr_read(addr + i, 1);
   else if(direction == DIFFTEST_TO_REF)
+  {
 	for(int i = 0; i < n; i ++)
+	{
 	  paddr_write(addr + i, 1, *(char*)(buf + i));
+  	  printf("copy memory at %08x\n", addr + i);
+	}
+  }
   else
 	assert(0);
 //  ref_difftest_memcpy(addr, buf, n, direction);
