bdbe1cc82e7bae871678b68cce591efc4a14d2ec
M_E662 <M_E662@noreply.gitcode.com>
2026-07-24 09:32:49 +0800

    给nemu添加了difftest相关api

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,13 +18,12 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
-extern void (*ref_difftest_regcpy)(void *dut, bool direction);
-extern void (*ref_difftest_exec)(uint64_t n);
-extern void (*ref_difftest_raise_intr)(uint64_t NO);
+//extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
+//extern void (*ref_difftest_regcpy)(void *dut, bool direction);
+//extern void (*ref_difftest_exec)(uint64_t n);
+//extern void (*ref_difftest_raise_intr)(uint64_t NO);
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  /*
   if(direction == DIFFTEST_TO_DUT)
 	for(int i = 0; i < n; i ++)
 	  *(char*)(buf + i) = paddr_read(addr + i, 1);
@@ -33,13 +32,11 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 	  paddr_write(addr + i, 1, *(char*)(buf + i));
   else
 	assert(0);
-	*/
-  ref_difftest_memcpy(addr, buf, n, direction);
+//  ref_difftest_memcpy(addr, buf, n, direction);
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  ref_difftest_regcpy(dut, direction);
-  /*
+//  ref_difftest_regcpy(dut, direction);
   word_t *p = (word_t*)&cpu;
   printf("test\n");
   if(direction == DIFFTEST_TO_DUT)
@@ -47,7 +44,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
 	for(int i = 0; i < DIFFTEST_REG_SIZE; i ++)
 	{
 	  printf("to dut cpu: %d, dut: %d\n", *(p + i), *(((word_t*)dut) + i));
-	  *(p + i) = *(((word_t*)dut) + i);
+	  *(((word_t*)dut) + i) = *(p + i);
 	}
 //	  cpu.gpr[i] = ctx->XPR[i];
 //	cpu.pc = ctx->pc;
@@ -57,21 +54,20 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
 	for(int i = 0; i < DIFFTEST_REG_SIZE; i ++)
 	{
 	  printf("to ref cpu: %d, dut: %d\n", *(p + i), *(((word_t*)dut) + i));
-	  *(((word_t*)dut) + i) = *(p + i);
+	  *(p + i) = *(((word_t*)dut) + i);
 	}
   }
   else
 	assert(0);
-	*/
-
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  ref_difftest_exec(n);
+//  ref_difftest_exec(n);
+  execute(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
-  ref_difftest_raise_intr(NO);
+//  ref_difftest_raise_intr(NO);
 }
 
 __EXPORT void difftest_init(int port) {
