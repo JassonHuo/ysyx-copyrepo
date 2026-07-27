243ac2f37aec236e202ef0868b69afddf0c3507f
tracer-ysyx <tracer@ysyx.org>
2026-07-27 16:51:43 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:51:43 up  8:03,  0 users,  load average: 0.02, 0.02, 0.00

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -50,7 +50,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.csr[0x342] = NO;	  //mcause	 
 #ifdef CONFIG_ETRACE
   char tmp[100] = "";
-  sprintf(tmp, "Trap happened at mepc: %08x, mcause: %d, targetPc: %08x", epc, NO, cpu.csr[0x305]);
+  extern CPU_state cpu;
+  sprintf(tmp, "Trap happened at pc: %08x, mepc: %08x, mcause: %d, targetPc: %08x", cpu.pc, epc, NO, cpu.csr[0x305]);
   et_inQue(tmp);
 #endif
   return cpu.csr[0x305];  //mtvec
