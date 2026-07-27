ed6ceafc5e1fb7ffd3b996948c34f9586806b285
tracer-ysyx <tracer@ysyx.org>
2026-07-27 11:57:56 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:57:56 up  3:09,  0 users,  load average: 1.35, 0.82, 0.32

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -36,7 +36,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   ctx->mepc = (uintptr_t)entry;
   ctx->mcause = 11;
   ctx->gpr[2] = (uintptr_t)ctx;
-  ctx->gpr[10] = (uintptr_t)kstack.end;
+  ctx->gpr[10] = (uintptr_t)ctx;
   return ctx;
 }
 
