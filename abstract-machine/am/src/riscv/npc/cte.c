a9c67dc7a104b9b972ec38d4ebab98f28dcdeaef
tracer-ysyx <tracer@ysyx.org>
2026-07-29 16:40:54 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:40:54 up  7:54,  0 users,  load average: 0.21, 0.28, 0.13

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -31,7 +31,12 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  Context* ctx = (Context*)(kstack.end - sizeof(Context));
+  ctx->mepc = (uintptr_t)entry;
+  ctx->mcause = 11;
+  ctx->mstatus = 0x1800;
+  ctx->gpr[10] = (uintptr_t)arg;
+  return ctx;
 }
 
 void yield() {
