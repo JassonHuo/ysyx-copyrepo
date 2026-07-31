d981ce46082bbc564503527d06c3b4a934a189f9
tracer-ysyx <tracer@ysyx.org>
2026-07-31 10:55:40 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:55:40 up  2:13,  0 users,  load average: 0.00, 0.00, 0.01

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -78,7 +78,6 @@ static void out_of_bound(paddr_t addr) {
 #ifdef CONFIG_MTRACE
   display_mt_buffer();
 #endif
-  return;
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
     addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 
