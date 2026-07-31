57b920ff6b83fad21b97b4d331621a44f8d1c1fb
tracer-ysyx <tracer@ysyx.org>
2026-07-31 15:59:32 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:59:32 up  7:17,  0 users,  load average: 0.01, 0.07, 0.08

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -258,7 +258,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   nemu_current_pc = s->pc;
 //  printf("%08x, %08x\n", s->pc, s->snpc);
   cpu.pc = s->dnpc;
-  printf("pc in nemu: %08x\n", s->pc);
+  printf("pc in nemu: %08x\n", cpu.pc);
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
