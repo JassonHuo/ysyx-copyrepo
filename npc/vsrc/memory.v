0f14d7740996d176e8a6343db13fe0b6aaa62b92
M_E662 <M_E662@noreply.gitcode.com>
2026-08-07 21:48:10 +0800

    访存握手逻辑存在冲突和错误待解决

--- npc/vsrc/memory.v
+++ npc/vsrc/memory.v
@@ -10,56 +10,47 @@ module memory(
   output reg [31: 0] rdata
 );
 
-  reg [9: 0] counter;
-  reg [9: 0] lfsr;
-  initial begin
-	lfsr = 10'b101;
-  end
-
-  always@(posedge clk)begin
-	lfsr = {lfsr[8: 0], lfsr[0] ^ lfsr[3] ^ lfsr[7] ^ lfsr[9]};
-  end
-
-  /*
   reg [31: 0] addr_reg;
   reg wen_reg;
   reg [31: 0] wdata_reg;
   reg [3: 0] mask_reg;
   reg reqValid_reg;
-  */
 
- /*
   always@(posedge clk)begin
 	addr_reg <= addr_reg;
 	wen_reg <= wen_reg;
 	wdata_reg <= wdata_reg;
 	mask_reg <= mask_reg;
-	reqValid_reg <= reqValid_reg;
 	if(reqValid)begin
 	  addr_reg <= addr;
 	  wen_reg <= wen;
 	  wdata_reg <= wdata;
 	  mask_reg <= mask;
-	  reqValid_reg <= reqValid;
 	end
-	if(respValid)
-	  reqValid_reg <= 1'b0;
   end
-  */
+
+  parameter MEM_IDLE = 0, MEM_WAITRESP = 1;
+  reg state, next_state;
+  always@(*)begin
+	case(state)
+	  MEM_IDLE: next_state = reqValid ? MEM_WAITRESP: MEM_IDLE;
+	  MEM_WAITRESP: next_state = respValid ? MEM_IDLE: MEM_WAITRESP;
+	  default: next_state = MEM_IDLE;
+	endcase
+  end
 
   always@(posedge clk)begin
-//	respValid <= 1'b0;
-//	counter <= (counter == 0) ? lfsr: counter - 1;
-	counter <= 0;
 	if(reqValid)begin
-	  counter <= (counter == 0) ? lfsr: counter - 1;
-	  if(~wen)begin
-		rdata <= pmem_read(addr);
-	  end
-	  else
-		pmem_write(addr, wdata, {4'b0, mask});
-//	  respValid <= (counter == 0) ? 1'b1: 1'b0;
+	  if(wen_reg)
+		pmem_write(addr_reg, wdata_reg, {4'b0, mask_reg});
 	end
+  end
+
+  always@(*)begin
+	if(respValid)
+	  rdata = pmem_read(addr_reg);
+	else
+	  rdata = 32'b0;
   end
   
 endmodule
