62d98c76b14d264953f487bbba06914ca7fd898d
tracer-ysyx <tracer@ysyx.org>
2026-08-07 19:58:02 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:58:02 up 11:03,  0 users,  load average: 0.03, 0.09, 0.04

--- npc/vsrc/memory.v
+++ npc/vsrc/memory.v
@@ -10,16 +10,6 @@ module memory(
   output reg [31: 0] rdata
 );
 
-  reg [9: 0] counter;
-  reg [9: 0] lfsr;
-  initial begin
-	lfsr = 10'b101;
-  end
-
-  always@(posedge clk)begin
-	lfsr = {lfsr[8: 0], lfsr[0] ^ lfsr[3] ^ lfsr[7] ^ lfsr[9]};
-  end
-
   /*
   reg [31: 0] addr_reg;
   reg wen_reg;
@@ -48,18 +38,14 @@ module memory(
   */
 
   always@(posedge clk)begin
-//	respValid <= 1'b0;
-//	counter <= (counter == 0) ? lfsr: counter - 1;
-	counter <= 0;
 	if(reqValid)begin
-	  counter <= (counter == 0) ? lfsr: counter - 1;
-	  if(~wen)begin
-		rdata <= pmem_read(addr);
-	  end
-	  else
+	  if(wen)
 		pmem_write(addr, wdata, {4'b0, mask});
-//	  respValid <= (counter == 0) ? 1'b1: 1'b0;
 	end
+  end
+
+  always@(*)begin
+	rdata = pmem_read(addr);
   end
   
 endmodule
