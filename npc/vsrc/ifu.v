da7a68eea6af51bd04c78c3fb6ea036f2e96cf68
tracer-ysyx <tracer@ysyx.org>
2026-08-08 09:56:55 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:56:55 up  1:00,  0 users,  load average: 0.36, 0.22, 0.11

--- npc/vsrc/ifu.v
+++ npc/vsrc/ifu.v
@@ -67,7 +67,7 @@ module ifu(
 	inst <= inst;
 	respValid <= reqValid;
 	counter <= 4'b0;
-	if(reqValid)begin
+	if(state == `IF_WAIT)begin
 //	  inst <= pmem_read(inst_addr);
 	  inst <= inst_in;
 	end
