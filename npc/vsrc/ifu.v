a33ec58dd89238672598228e88152c33ab756f17
tracer-ysyx <tracer@ysyx.org>
2026-08-08 09:51:15 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:51:15 up 54 min,  0 users,  load average: 0.61, 0.21, 0.07

--- npc/vsrc/ifu.v
+++ npc/vsrc/ifu.v
@@ -31,7 +31,7 @@ module ifu(
 	else
 	  case(state)
 		`IF_IDLE: next_state = `IF_WAIT;
-		`IF_WAIT: next_state = respValid_tmp ? `IF_RUNNING: (done ? `IF_WAIT: `IF_IDLE);
+		`IF_WAIT: next_state = respValid_tmp ? `IF_RUNNING: `IF_WAIT;
 		`IF_RUNNING: next_state = done ? `IF_IDLE: `IF_RUNNING;
 		default: next_state = `LS_IDLE;
 	  endcase
