615c149c4b87c6ba251cea63734604df5b521418
M_E662 <M_E662@noreply.gitcode.com>
2026-08-09 21:23:27 +0800

    为访存模块添加了状态机，if和ls模块待适配相应功能

--- npc/vsrc/memory.v
+++ npc/vsrc/memory.v
@@ -1,7 +1,7 @@
 module memory(
   input clk,
   input reqValid,
-  input reqReady,
+  output reqReady,
   input [31: 0] addr,
   input wen,
   input [31: 0] wdata,
@@ -11,17 +11,24 @@ module memory(
   output reg [31: 0] rdata
 );
 
-/*
-  parameter MEM_IDLE = 0, MEM_WAITRESP = 1;
-  reg state, next_state;
+  parameter IDLE = 0, REQREADY = 1, WAIT = 2, RESPVALID = 3;
+  reg [2: 0] state, next_state;
   always@(*)begin
 	case(state)
+	  /*
 	  MEM_IDLE: next_state = reqValid ? MEM_WAITRESP: MEM_IDLE;
 	  MEM_WAITRESP: next_state = respValid ? MEM_IDLE: MEM_WAITRESP;
 	  default: next_state = MEM_IDLE;
+	  */
+	  IDLE: next_state = reqValid ? REQREADY: IDLE;
+	  REQREADY: next_state = respValid ? RESPVALID: WAIT;
+	  WAIT: next_state = respValid ? RESPVALID: WAIT;
+	  RESPVALID: next_state = respReady ? IDLE: RESPVALID;
+	  default: next_state = IDLE;
 	endcase
   end
-  */
+
+  assign reqReady = (state == REQREADY);
 
   always@(posedge clk)begin
 	if(reqValid)begin
