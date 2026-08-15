3902552a4e3a24b99d525e308e55f34679e73fef
M_E662 <M_E662@noreply.gitcode.com>
2026-08-15 15:28:56 +0800

    添加了一些am对soc的支持，存在地址偏移bug导致程序尝试写入rom

---
+++ abstract-machine/am/src/riscv/ysyxsoc/cte.c
@@ -0,0 +1,62 @@
+#include <am.h>
+#include <riscv/riscv.h>
+#include <klib.h>
+
+static Context* (*user_handler)(Event, Context*) = NULL;
+
+Context* __am_irq_handle(Context *c) {
+  if (user_handler) {
+    Event ev = {0};
+    switch (c->mcause) {
+	  case 11:
+		c->mepc += 4;
+		  ev.event = EVENT_YIELD; break;
+      default: ev.event = EVENT_ERROR; break;
+    }
+
+//	printf("irq handler: %08p\n", user_handler);
+    c = user_handler(ev, c);
+    assert(c != NULL);
+  }
+  // assert((uintptr_t)c >= 0x80000000);
+//  printf("c: %p\n", c);
+  return c;
+}
+
+extern void __am_asm_trap(void);
+
+bool cte_init(Context*(*handler)(Event, Context*)) {
+  // initialize exception entry
+  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+
+  // register event handler
+  user_handler = handler;
+//  printf("init handler: %08p\n", user_handler);
+
+  return true;
+}
+
+Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
+  Context* ctx = (Context*)(kstack.end - sizeof(Context));
+  ctx->mepc = (uintptr_t)entry;
+  ctx->mcause = 11;
+  ctx->mstatus = 0x1800;
+  ctx->gpr[10] = (uintptr_t)arg;
+//  printf("creat: %p\n", ctx);
+  return ctx;
+}
+
+void yield() {
+#ifdef __riscv_e
+  asm volatile("li a5, -1; ecall");
+#else
+  asm volatile("li a7, -1; ecall");
+#endif
+}
+
+bool ienabled() {
+  return false;
+}
+
+void iset(bool enable) {
+}
