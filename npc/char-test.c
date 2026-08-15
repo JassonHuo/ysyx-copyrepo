3b69800332602d97143325d75df09affa3b90a31
M_E662 <M_E662@noreply.gitcode.com>
2026-08-15 14:08:31 +0800

    进行了uart输出测试

--- npc/char-test.c
+++ npc/char-test.c
@@ -1,7 +1,7 @@
-#define UART_BASE 0x02000000L
+#define UART_BASE 0x10000000
 #define UART_TX   0
 void _start() {
   *(volatile char *)(UART_BASE + UART_TX) = 'A';
-  *(volatile char *)(UART_BASE + UART_TX) = '\n';
+//  *(volatile char *)(UART_BASE + UART_TX) = '\n';
   while (1);
 }
