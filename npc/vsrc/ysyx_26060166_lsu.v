d929cc8bea50500cfd8fe4d16edccd5bbf584b27
tracer-ysyx <tracer@ysyx.org>
2026-08-18 13:06:50 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:06:50 up 12:08,  0 users,  load average: 1.11, 0.52, 0.25

--- npc/vsrc/ysyx_26060166_lsu.v
+++ npc/vsrc/ysyx_26060166_lsu.v
@@ -173,12 +173,9 @@ module ysyx_26060166_lsu(
 
   always@(*)begin
 	if(mem_valid & valid_aft)begin
-	  /*
 	  rdata = (lsu_rdata >> {alu[1: 0], 3'b0}) & (width == `MEM_WORD ? ~32'b0:
 		(width == `MEM_HALF ? 32'hFFFF:
 		(width == `MEM_BYTE ? 32'hFF: 32'b0)));
-	  */
-	  rdata = lsu_rdata;
 	  rdata = rdata | (width == `MEM_HALF ? {{16{is_signed & rdata[15]}}, 16'b0}:
 		(width == `MEM_BYTE ? {{24{is_signed & rdata[7]}}, 8'b0}: 32'b0));
 //	  rdata = lsu_rdata;
