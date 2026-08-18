28fc1cf27e2417905da2f904cf2431b8708da86a
tracer-ysyx <tracer@ysyx.org>
2026-08-18 09:56:43 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:56:43 up  8:58,  0 users,  load average: 0.32, 0.17, 0.11

--- npc/vsrc/ysyx_26060166_xbar.v
+++ npc/vsrc/ysyx_26060166_xbar.v
@@ -137,9 +137,9 @@ module ysyx_26060166_xbar(
   wire [3:0] rid;
 
   always@(*)begin
-	if(araddr <= 32'h20000000 || araddr >= 32'h0fffffff || araddr <= 32'h20000fff && araddr >= 32'h0fffffff)
+	if(araddr >= 32'h20000000 || araddr <= 32'h0fffffff || araddr <= 32'h20000fff && araddr >= 32'h0fffffff)
 	  TO_device();
-	if(awaddr <= 32'h20000000 || awaddr >= 32'h0fffffff || awaddr <= 32'h20000fff && awaddr >= 32'h0fffffff)
+	if(awaddr >= 32'h20000000 || awaddr <= 32'h0fffffff || awaddr <= 32'h20000fff && awaddr >= 32'h0fffffff)
 	  TO_device();
   end
 
