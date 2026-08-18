b34d26e1b0306b47b7bc7beda87ae75a90e03a67
tracer-ysyx <tracer@ysyx.org>
2026-08-18 10:06:12 +0800

    > sim RTL ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:06:12 up  9:08,  0 users,  load average: 0.17, 0.10, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -546,6 +546,8 @@ void run_cycle(uint64_t n)
 #endif
 	top->clock = 1;
 	top->eval();
+	if(to_device)
+	  printf("test\n");
 #ifdef CONFIG_WAVE
 	tfp->dump(contextp->time());
 	contextp->timeInc(1);
