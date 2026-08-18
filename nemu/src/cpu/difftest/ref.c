2c7dc5127fee044a77b08a5dfd3bbc05594eee79
tracer-ysyx <tracer@ysyx.org>
2026-08-18 12:10:50 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:10:50 up 11:12,  0 users,  load average: 0.11, 0.15, 0.10

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -70,6 +70,7 @@ __EXPORT void difftest_raise_intr(word_t NO) {
 __EXPORT void difftest_init(int port) {
   void init_mem();
   init_mem();
+  printf("%08x\n", paddr_read(0x20000000 + 4, 4));
   /* Perform ISA dependent initialization. */
   init_isa();
 }
