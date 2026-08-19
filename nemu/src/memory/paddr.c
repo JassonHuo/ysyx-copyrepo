0aa24ae1c092fd0e96833ac5da539e014dc3f0ea
tracer-ysyx <tracer@ysyx.org>
2026-08-19 16:48:58 +0800

    > compile NEMU ysyx_26060166 霍天晟 Linux LAPTOP-HULHIN98 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:48:58 up  3:10,  0 users,  load average: 1.76, 0.92, 0.42

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -99,10 +99,10 @@ static word_t pmem_read(paddr_t addr, int len) {
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
-  if(addr >= 0x0f000000 && addr <= 0x0f000fff)
-  {
 	if(addr == 0x0f001ffc)
 	  printf("write %08x\n", addr);
+  if(addr >= 0x0f000000 && addr <= 0x0f000fff)
+  {
 	switch(len)
 	{
 	  case 1:
